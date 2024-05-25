62c66c52c79fa3e08c065647bb5095033e6e4ff5
Suvansarkar <suvansarkar1@gmail.com>
2024-05-26 00:19:19 +0530

    Add route support for external and internal links

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,26 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import ErrorPage from './components/ErrorPage.jsx'
+import RedirectHandler from './utils/RedirectHandler.jsx'
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    errorElement: < ErrorPage />
+  },
+  {
+    path: "/*",
+    element: < RedirectHandler />
+  },
+])
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
---
+++ src/utils/RedirectHandler.jsx
@@ -0,0 +1,23 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+import redirects from "../redirects.json";
+import ErrorPage from "../components/ErrorPage";
+
+const RedirectHandler = () => {
+  const location = useLocation();
+
+  useEffect(() => {
+    const path = location.pathname.substring(1); // Remove the leading '/'
+
+    const redirectTo = redirects[path];
+
+    if (redirectTo) {
+      window.location.href = redirectTo; // Redirect to the external link
+    }
+    // If no match is found, do nothing and let the router render the original path
+  }, [location]);
+
+  return <ErrorPage />;
+};
+
+export default RedirectHandler;
