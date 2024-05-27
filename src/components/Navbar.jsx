2a194e22abdec64b9655ebd00067f202ba1c893a
Suvansarkar <suvansarkar1@gmail.com>
2024-05-27 17:05:29 +0530

    Make prettay

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,24 +26,29 @@ export default function Navbar() {
   )
   return (
     <div className="w-full fixed flex">
-      <div className="disappear w-full top-0 right-0 z-10 text-white bg-black h-20 border-[#161616] flex p-3 justify-around items-center" >
-        <div className="h-full pl-8">
+      <div className="disappear w-full top-0 right-0 z-10 text-white bg-black h-20 border-[#161616] flex p-3 justify-between items-center" >
+        <div className="h-full pl-8 w-full">
           <img src="/web3ssh.png" alt="Web3SSH logo" className="max-h-full" />
         </div>
-        <div className="w-full flex justify-center gap-6">
+        <div className="flex justify-between gap-6 items-center ">
           <ul className="flex gap-6">
-            <li><a href="#" >Home</a></li>
-            <li><a href="#" >Sponsors</a></li>
-            <li><a href="#" >Schedule</a></li>
-            <li><a href="#" >FAQ</a></li>
+            <li><a href="#" className="hover:bg-[#AE79FF] transition-colors duration-300 px-3 py-1 rounded-md">Home</a></li>
+            <li><a href="#" className="hover:bg-[#AE79FF] transition-colors duration-300 px-3 py-1 rounded-md">Sponsors</a></li>
+            <li><a href="#" className="hover:bg-[#AE79FF] transition-colors duration-300 px-3 py-1 rounded-md">Schedule</a></li>
+            <li><a href="#" className="hover:bg-[#AE79FF] transition-colors duration-300 px-3 py-1 rounded-md">FAQ</a></li>
           </ul>
+        </div>
+        <div className="flex justify-end items-center gap-6 w-full">
           <ul className="flex gap-6">
-            <li>< Whatsapp fill="#fff" width="20px" height="20px" className="hover:fill-[#291549]" /></li>
-            {/*LINKED IN IS NOT WORKING FOR SOME GODFORSAKEN REASON*/}
-            {/*<li>< LinkedIn fill="#fff" width="20px" height="20px" className="hover:fill-[#291549]" /></li>*/}
-            <li>< Twitter fill="#fff" width="20px" height="20px" className="hover:fill-[#291549]" /></li>
-            <li>< Instagram fill="#fff" width="20px" height="20px" className="hover:fill-[#291549]" /></li>
+            <li><a href="/whatsapp" >< Whatsapp fill="#fff" width="20px" height="20px" className="hover:fill-[#AE79FF]" /></a></li>
+            {/*L<a href="" />INKED IN IS NOT WORKING FOR SOME GODFORSAKEN REASON*/}
+            <li><a href="https://www.linkedin.com/company/web3ssh/" target="_blank">< LinkedIn fill="#fff" width="20px" height="20px" className="hover:fill-[#AE79FF]" /></a></li>
+            <li><a href="https://x.com/web3ssh" target="_blank">< Twitter fill="#fff" width="20px" height="20px" className="hover:fill-[#AE79FF]" /></a></li>
+            <li><a href="https://www.instagram.com/web3ssh/" target="_blank">< Instagram fill="#fff" width="20px" height="20px" className="hover:fill-[#AE79FF]" /></a></li>
           </ul>
+          <a href="early-iiits">
+            <button type="button" className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2">Register Now!</button>
+          </a>
         </div>
       </div>
 
