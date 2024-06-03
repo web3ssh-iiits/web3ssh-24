2f247377a39dde18e811073fcd41ef9b35087b4e
Mithun.U <116871732+Mithun-750@users.noreply.github.com>
2024-06-03 17:42:06 +0530

    outcomes added

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import Navbar from "./components/Navbar";
 import CommonQuestions from "./components/CommonQuestions";
 import HeroSection from "./components/HeroSection";
 import Footer from "./components/Footer";
+import Outcomes from "./components/Outcomes";
 
 function App() {
     return (
         <>
             <Navbar />
             <HeroSection className="pt-28" />
+            <Outcomes />
             <CommonQuestions />
             <Footer />
         </>
