1846d51561e6bd3cdf8ff145fcea6ea5d3abc49f
Suvansarkar <suvansarkar1@gmail.com>
2024-06-04 23:29:00 +0530

    NABAR JUST GOT UPDATES!!!

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -19,7 +19,6 @@ export default function Navbar() {
         },
         margin: '20 90',
         backgroundColor: 'rgba(0, 0, 0, 0.61)',
-        border: '1px solid #32224B',
         borderRadius: '20px',
         backdropFilter: 'blur(5px)',
       });
@@ -27,7 +26,7 @@ export default function Navbar() {
   });
   return (
     <div className="w-full fixed top-0 flex z-50">
-      <div className="disappear w-full right-0 z-10 text-white bg-[#221040] h-20 border-[#32224B] flex p-3 justify-between items-center">
+      <div className="disappear w-full right-0 z-10 text-white h-20 border-none flex p-3 justify-between items-center">
         <div className="h-full pl-8 w-full">
           <img
             src="/web3ssh.png"
@@ -123,7 +122,7 @@ export default function Navbar() {
               type="button"
               className="text-white bg-gradient-to-br from-purple-600 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center me-2"
             >
-              Register Now!
+              Register Now
             </button>
           </a>
         </div>
