4f7a18d5f933d0eee6fcc09dc680da87fdfc1bab
aahnik <daw@aahnik.dev>
2024-06-08 02:53:58 +0530

    Make analytics section responsive

--- src/components/AnalyticsCard.jsx
+++ src/components/AnalyticsCard.jsx
@@ -1,11 +1,11 @@
 export default function AnalyticsCard({ icon, number, unit, title }) {
   return (
     <>
-      <div className="mx-12">
+      <div className="mx-12 w-full p-2 py-8 flex-1 ">
         <div className="flex w-72">
-          <div className="flex w-full max-w-full flex-col break-words rounded-lg  bg-white bg-opacity-50 text-gray-600 shadow-lg">
+          <div className="flex w-full max-w-full flex-col break-words rounded-lg  bg-black bg-opacity-30 text-white shadow-lg">
             <div className="p-4">
-              <div className="absolute -mt-8 ml-[235px] bg-white bg-opacity-50 h-16 w-16 rounded-[32px] text-center text-white shadow-lg">
+              <div className="absolute -mt-8 ml-[235px] bg-black bg-opacity-60 h-16 w-16 rounded-[32px] text-center text-white shadow-lg">
                 {icon}
               </div>
               <div className="pt-1 text-left">
--- src/components/AnalyticsSection.jsx
+++ src/components/AnalyticsSection.jsx
@@ -3,13 +3,19 @@ import AnalyticsCard from './AnalyticsCard';
 export default function AnalyticsSection() {
   return (
     <>
-      <div className="flex m-12 justify-center">
-        <AnalyticsCard />
-        <AnalyticsCard />
-      </div>
-      <div className="flex m-12 justify-center">
-        <AnalyticsCard />
-        <AnalyticsCard />
+      <div className="flex m-12 justify-center flex-wrap mx-auto p-4">
+        <AnalyticsCard
+          number={'+300'}
+          title={'Online Participants'}
+          unit={'students'}
+        />
+        <AnalyticsCard
+          number={'+60'}
+          title={'Offline Participants'}
+          unit={'students'}
+        />
+        <AnalyticsCard number={'+20'} unit={'experts'} title={'Speakers'} />
+        <AnalyticsCard number={'+4'} unit={'companies'} title={'Sponsors'} />
       </div>
     </>
   );
