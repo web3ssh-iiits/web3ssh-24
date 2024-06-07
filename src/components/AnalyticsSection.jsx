e182e421561d3416309ef8fdc723cdc06e055a54
aahnik <daw@aahnik.dev>
2024-06-08 03:19:21 +0530

    Decouple analytics data from component

--- src/components/AnalyticsSection.jsx
+++ src/components/AnalyticsSection.jsx
@@ -1,21 +1,18 @@
 import AnalyticsCard from './AnalyticsCard';
+import analyticsData from '../data/analytics.json';
 
 export default function AnalyticsSection() {
   return (
     <>
       <div className="flex m-12 justify-center flex-wrap mx-auto p-4">
-        <AnalyticsCard
-          number={'+300'}
-          title={'Online Participants'}
-          unit={'students'}
-        />
-        <AnalyticsCard
-          number={'+60'}
-          title={'Offline Participants'}
-          unit={'students'}
-        />
-        <AnalyticsCard number={'+20'} unit={'experts'} title={'Speakers'} />
-        <AnalyticsCard number={'+4'} unit={'companies'} title={'Sponsors'} />
+        {analyticsData.map((data, index) => (
+          <AnalyticsCard
+            key={index}
+            number={data.number}
+            title={data.title}
+            unit={data.unit}
+          />
+        ))}
       </div>
     </>
   );
