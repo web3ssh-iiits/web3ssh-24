ec49441e9fadf921f05bbad0adfe08857382867d
Mithun.U <116871732+Mithun-750@users.noreply.github.com>
2024-06-13 23:56:19 +0530

    add images and integrate with json file

--- src/components/KeyComponents.jsx
+++ src/components/KeyComponents.jsx
@@ -1,4 +1,4 @@
-import React, { useRef, useState, useLayoutEffect, useMemo } from 'react';
+import React, { useRef, useState, useLayoutEffect } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import data from '../data/keyComponents.json'; // Importing the JSON data directly
@@ -31,18 +31,27 @@ const KeyComponents = () => {
     }, [data]);
 
     return (
-        <section ref={horContainer} className="flex w-screen min-h-screen bg-gray-950/40 text-gray-300 flex-col border-b-2">
+        <section ref={horContainer} className="flex w-screen h-screen bg-gray-950/40 text-gray-300 flex-col border-b-2">
             <div className='w-screen h-[300px] flex items-center justify-center border-b-2 text-7xl backdrop-blur-md'>
                 Key Components
             </div>
-            <div className='w-full  flex h-[calc(100vh-300px)] overflow-x-hidden '>
+            <div className='w-full flex h-[calc(100vh-300px)] overflow-x-hidden '>
                 <div ref={horText} className='flex h-full'>
                     {data.map((content, index) => (
-                        <div key={index} className="h-full w-screen flex items-center justify-center px-8 border-x text-2xl backdrop-blur-sm">
-                            <div>
-                                <h2 className="text-4xl mb-4">{content.title}</h2>
-                                <p>{content.text}</p>
-                                {content.logoUrl && <img src={content.logoUrl} alt={content.title} className="mt-4" />}
+                        <div key={index} className="group h-full w-screen flex flex-wrap items-center justify-evenly px-8 border-x text-2xl backdrop-blur-sm">
+                            {content.graphics &&
+                                <div
+                                    className="relative inline-block overflow-hidden rounded-xl px-8 py-3 text-sm font-medium bg-gray-900/30 group-hover:bg-white/70 transition-all duration-500 delay-100">
+                                    <span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-white/70 transition-all duration-200 group-hover:w-full"></span>
+                                    <span className="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-white/70 transition-all duration-200 group-hover:h-full"></span>
+                                    <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-white/70 transition-all duration-200 group-hover:w-full"></span>
+                                    <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-white/70 transition-all duration-200 group-hover:h-full"></span>
+                                    <img src={content.graphics} alt={content.title} className=" w-[250px] h-[250px] " />
+                                </div>
+                            }
+                            <div className=" text-left">
+                                <h2 className="text-4xl font-bold mb-4">{content.title}</h2>
+                                <p className="mb-4">{content.text}</p>
                             </div>
                         </div>
                     ))}
--- src/components/Outcomes.jsx
+++ src/components/Outcomes.jsx
@@ -1,6 +1,8 @@
-import React, { useRef, useState, useLayoutEffect, useMemo } from 'react';
+import React, { useRef, useLayoutEffect } from 'react';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import data from '../data/outcomes.json'; // Importing the JSON data directly
+
 gsap.registerPlugin(ScrollTrigger);
 
 const Outcomes = () => {
@@ -8,19 +10,6 @@ const Outcomes = () => {
     const text = useRef(null);
     const horContainer = useRef(null);
     const horText = useRef(null);
-
-    const data = useMemo(() => [
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente ut vero eveniet! Quas impedit corrupti consectetur officia vitae placeat! Voluptas inventore nam rem est iusto sunt quibusdam, similique eum dolorem.',
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis expedita numquam sequi deleniti, rem modi ut, culpa itaque dignissimos eveniet fugiat in dolores non sit dolorum. Neque in ipsum corporis.',
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis expedita numquam sequi deleniti, rem modi ut, culpa itaque dignissimos eveniet fugiat in dolores non sit dolorum. Neque in ipsum corporis.',
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis expedita numquam sequi deleniti, rem modi ut, culpa itaque dignissimos eveniet fugiat in dolores non sit dolorum. Neque in ipsum corporis.',
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis expedita numquam sequi deleniti, rem modi ut, culpa itaque dignissimos eveniet fugiat in dolores non sit dolorum. Neque in ipsum corporis.',
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis expedita numquam sequi deleniti, rem modi ut, culpa itaque dignissimos eveniet fugiat in dolores non sit dolorum. Neque in ipsum corporis.',
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis expedita numquam sequi deleniti, rem modi ut, culpa itaque dignissimos eveniet fugiat in dolores non sit dolorum. Neque in ipsum corporis.',
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis expedita numquam sequi deleniti, rem modi ut, culpa itaque dignissimos eveniet fugiat in dolores non sit dolorum. Neque in ipsum corporis.',
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis expedita numquam sequi deleniti, rem modi ut, culpa itaque dignissimos eveniet fugiat in dolores non sit dolorum. Neque in ipsum corporis.',
-        'Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis expedita numquam sequi deleniti, rem modi ut, culpa itaque dignissimos eveniet fugiat in dolores non sit dolorum. Neque in ipsum corporis.'
-    ], []);
 
     useLayoutEffect(() => {
         const context = gsap.context(() => {
@@ -37,7 +26,6 @@ const Outcomes = () => {
 
             // Pin the horizontal container
             if ((window.innerWidth < 768)) {
-
                 let sections = gsap.utils.toArray(horText.current.children);
 
                 gsap.to(sections, {
@@ -58,37 +46,64 @@ const Outcomes = () => {
         };
     }, [data]);
 
-
     return (
         <>
-            {(window.innerWidth >= 768) && (<section className="flex w-screen mx-auto min-h-screen bg-gray-950/40 text-gray-300  overflow-hidden " ref={container}>
-                <div className='w-1/2 h-screen flex items-center justify-center border-r-2 text-7xl backdrop-blur-md' ref={text}>
-                    Outcomes
-                </div>
-                <div className='w-1/2 my-[100px]'>
-                    {data.map((content, index) => (
-                        <div key={index} className={`min-h-[300px] flex items-center justify-center content-div py-4 px-8 border-y text-2xl backdrop-blur-sm`}>
-                            {content}
-                        </div>
-                    ))}
-                </div>
-            </section>)}
-            {(window.innerWidth < 768) && (<section ref={horContainer} className="flex w-screen min-h-screen bg-gray-950/40 text-gray-300 flex-col border-b-2">
-                <div className='w-screen h-[200px] flex items-center justify-center border-b-2 text-7xl backdrop-blur-md'>
-                    Outcomes
-                </div>
-                <div className='w-full px-[100px] flex h-[calc(100vh-200px)] overflow-x-hidden '>
-                    <div ref={horText} className='flex h-full'>
+            {(window.innerWidth >= 768) && (
+                <section className="flex w-screen mx-auto min-h-screen bg-gray-950/40 text-gray-300 overflow-hidden" ref={container}>
+                    <div className='w-1/2 h-screen flex items-center justify-center border-r-2 text-7xl backdrop-blur-md' ref={text}>
+                        Outcomes
+                    </div>
+                    <div className='w-1/2 my-[100px]'>
                         {data.map((content, index) => (
-                            <div key={index} className="h-full min-w-[300px] flex items-center justify-center px-8 border-x text-2xl backdrop-blur-sm">
-                                {content}
+                            <div key={index} className="group min-h-[300px] flex w-full items-center justify-start gap-4 content-div py-4 px-8 border-y text-2xl backdrop-blur-sm">
+                                {content.graphics && (
+                                    <div
+                                        className="relative inline-block overflow-hidden rounded-xl px-8 py-3 text-sm font-medium bg-gray-900/30  group-hover:bg-white/70 transition-all duration-500 delay-100">
+                                        <span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-white/70 transition-all duration-200 group-hover:w-full"></span>
+                                        <span className="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-white/70 transition-all duration-200 group-hover:h-full"></span>
+                                        <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-white/70 transition-all duration-200 group-hover:w-full"></span>
+                                        <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-white/70 transition-all duration-200 group-hover:h-full"></span>
+                                        <img src={content.graphics} alt={content.title} className=" w-[150px] h-[150px] " />
+                                    </div>
+                                )}
+                                <div className=" text-left max-w-[630px]">
+                                    <h2 className="text-4xl font-bold mb-4">{content.title}</h2>
+                                    <p className="mb-4">{content.text}</p>
+                                </div>
                             </div>
                         ))}
                     </div>
-                </div>
-            </section>)}
-
-
+                </section>
+            )}
+            {(window.innerWidth < 768) && (
+                <section ref={horContainer} className="flex w-screen min-h-screen bg-gray-950/40 text-gray-300 flex-col border-b-2">
+                    <div className='w-screen h-[200px] flex items-center justify-center border-b-2 text-7xl backdrop-blur-md'>
+                        Outcomes
+                    </div>
+                    <div className='w-full px-[100px] flex h-[calc(100vh-200px)] overflow-x-hidden'>
+                        <div ref={horText} className='flex h-full'>
+                            {data.map((content, index) => (
+                                <div key={index} className="group h-full min-w-[300px] flex flex-col items-center justify-evenly px-8 border-x text-2xl backdrop-blur-sm">
+                                    {content.graphics && (
+                                        <div
+                                            className="relative inline-block overflow-hidden rounded-xl px-8 py-3 text-sm font-medium bg-gray-900/30 transition-all duration-500 delay-100">
+                                            <span className="ease absolute left-0 top-0 h-0 w-0 border-t-2 border-white/70 transition-all duration-200 group-hover:w-full"></span>
+                                            <span className="ease absolute right-0 top-0 h-0 w-0 border-r-2 border-white/70 transition-all duration-200 group-hover:h-full"></span>
+                                            <span className="ease absolute bottom-0 right-0 h-0 w-0 border-b-2 border-white/70 transition-all duration-200 group-hover:w-full"></span>
+                                            <span className="ease absolute bottom-0 left-0 h-0 w-0 border-l-2 border-white/70 transition-all duration-200 group-hover:h-full"></span>
+                                            <img src={content.graphics} alt={content.title} className=" w-[150px] h-[150px] " />
+                                        </div>
+                                    )}
+                                    <div className=" text-left max-w-[630px]">
+                                        <h2 className="text-4xl font-bold mb-4">{content.title}</h2>
+                                        <p className="mb-4">{content.text}</p>
+                                    </div>
+                                </div>
+                            ))}
+                        </div>
+                    </div>
+                </section>
+            )}
         </>
     );
 };
