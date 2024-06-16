54eea544fb339c38f76e06a242d634011a970452
aahnik <daw@aahnik.dev>
2024-06-17 04:28:29 +0530

    Add WhyJoin with aceternity FlipWord

---
+++ src/components/WhyJoin.jsx
@@ -0,0 +1,19 @@
+import { FlipWords } from './ui/flip-words';
+
+export function JoinUs() {
+  const words = [
+    'Networking',
+    'Learning',
+    'Passion               ',
+    'Guidance',
+  ];
+  return (
+    <div className="h-[20rem] flex justify-center items-center px-4">
+      <div className="text-6xl mx-auto font-semibold text-gray-100">
+        Experience
+        <FlipWords words={words} /> <br />
+        like never before!
+      </div>
+    </div>
+  );
+}
