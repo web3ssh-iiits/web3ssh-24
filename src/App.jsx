99c2c5d71fa4cdd980a1bfa72b460544982e9af5
aahnik <daw@aahnik.dev>
2024-06-17 05:03:14 +0530

    Update App.jsx with new components, and geist

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,29 @@
-
 import Navbar from './components/Navbar';
 import CommonQuestions from './components/CommonQuestions';
 import HeroSection from './components/HeroSection';
 import Footer from './components/Footer';
-import Outcomes from "./components/Outcomes";
+import Outcomes from './components/Outcomes';
 import GradientBackground from './components/background';
 import AnalyticsSection from './components/AnalyticsSection';
 import KeyComponents from './components/KeyComponents';
+import { Hackathon } from './components/Hackathon';
+import { JoinUs } from './components/WhyJoin';
 
 function App() {
   return (
-    <div className="relative">
+    <div className="relative font-geist">
       <GradientBackground />
       <div className="relative z-10">
         <Navbar />
-        <HeroSection className="pt-28" />
-        <AnalyticsSection />
-        <Outcomes />
-        <CommonQuestions />
+        <HeroSection />
+        {/* partners and sponsors */}
+        {/* speakers */}
         <KeyComponents />
+        <Hackathon />
+        <Outcomes />
+        <AnalyticsSection />
+        <JoinUs />
+        <CommonQuestions />
         <Footer />
       </div>
     </div>
