9c271bcae67063ae7ec9b313a8f4fb191a174db9
aahnik <daw@aahnik.dev>
2024-06-17 08:32:31 +0530

    Fix dark mode issues in Team page

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -9,7 +9,7 @@ export default function TeamPage() {
         {teamData.map((role, index) => (
           <>
             <div key={index} className="container px-6 py-10 mx-auto">
-              <h1 className="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-5xl dark:text-white">
+              <h1 className="text-2xl font-semibold text-center  capitalize lg:text-5xltext-white">
                 {role.role}
               </h1>
               <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 xl:grid-cols-4 items-center">
@@ -27,7 +27,7 @@ export default function TeamPage() {
                         }
                         alt="member profile pic"
                       />
-                      <h1 className="mt-10 text-2xl font-semibold text-gray-700 capitalize dark:text-white group-hover:text-white text-center">
+                      <h1 className="mt-10 text-2xl font-semibold  capitalize text-white group-hover:text-white text-center">
                         {member.name}
                       </h1>
                     </GlareCard>
