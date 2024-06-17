79983d2d8acf75e4d3479ae46df2255ef2d9417c
aahnik <daw@aahnik.dev>
2024-06-17 09:01:17 +0530

    Fix team page dark mode issue
    also remove unused import

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -1,4 +1,3 @@
-import { m } from 'framer-motion';
 import { GlareCard } from '../components/ui/glare-card';
 import teamData from '../data/team.json';
 
@@ -9,7 +8,7 @@ export default function TeamPage() {
         {teamData.map((role, index) => (
           <>
             <div key={index} className="container px-6 py-10 mx-auto">
-              <h1 className="text-2xl font-semibold text-center  capitalize lg:text-5xltext-white">
+              <h1 className="text-2xl font-semibold text-center  capitalize lg:text-5xl text-white">
                 {role.role}
               </h1>
               <div className="grid grid-cols-1 gap-8 mt-8 xl:mt-16 md:grid-cols-2 xl:grid-cols-4 items-center">
