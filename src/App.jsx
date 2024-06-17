2bdf028bc6b0c4535bcd95876d9e0135a611a9c9
aahnik <daw@aahnik.dev>
2024-06-17 06:02:05 +0530

    Move routing logic from main.jsx to App.jsx because pages will share common background, Navbar and Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import Navbar from './components/Navbar';
-import CommonQuestions from './components/CommonQuestions';
-import HeroSection from './components/HeroSection';
-import Footer from './components/Footer';
-import Outcomes from './components/Outcomes';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import RedirectHandler from './utils/RedirectHandler';
+import HomePage from './pages/Home';
 import GradientBackground from './components/background';
-import AnalyticsSection from './components/AnalyticsSection';
-import KeyComponents from './components/KeyComponents';
-import { Hackathon } from './components/Hackathon';
-import { JoinUs } from './components/WhyJoin';
+import Footer from './components/Footer';
+import TeamPage from './pages/Team.jsx';
+import JoinCodePage from './pages/Join.jsx';
 
 function App() {
   return (
@@ -15,15 +13,14 @@ function App() {
       <GradientBackground />
       <div className="relative z-10">
         <Navbar />
-        <HeroSection />
-        {/* partners and sponsors */}
-        {/* speakers */}
-        <KeyComponents />
-        <Hackathon />
-        <Outcomes />
-        <AnalyticsSection />
-        <JoinUs />
-        <CommonQuestions />
+        <Router>
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/team" element={<TeamPage />} />
+            <Route path="/join" element={<JoinCodePage />} />
+            <Route path="/*" element={<RedirectHandler />} />
+          </Routes>
+        </Router>
         <Footer />
       </div>
     </div>
