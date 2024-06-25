9e7527a7f8e8eb2e6f4fe4ae857e1f9b1c789d13
aahnik <daw@aahnik.dev>
2024-06-25 23:53:19 +0530

    Remove accidental blank space from WhyJoin.jsx

--- src/components/WhyJoin.jsx
+++ src/components/WhyJoin.jsx
@@ -1,12 +1,7 @@
 import { FlipWords } from './ui/flip-words';
 
 export function JoinUs() {
-  const words = [
-    'Networking',
-    'Learning',
-    'Passion               ',
-    'Guidance',
-  ];
+  const words = ['Networking', 'Learning', 'Passion', 'Guidance'];
   return (
     <div className="h-[20rem] flex justify-center items-center px-4">
       <div className="text-6xl mx-auto font-semibold text-gray-100">
