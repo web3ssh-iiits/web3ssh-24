c7b0c8ca64b18ef14bfbd2a0099437984e79aacd
aahnik <daw@aahnik.dev>
2024-06-26 01:49:53 +0530

    Fix mobile look of join page

--- src/app/join/page.tsx
+++ src/app/join/page.tsx
@@ -59,49 +59,51 @@ function DiscountUnlocked() {
     <div className="flex flex-col items-center justify-center h-screen text-center">
       {isValid ? (
         <>
-          <div className="p-6 border mx-8 rounded-lg shadow-md bg-white bg-opacity-10 mt-32">
-            <Confetti numberOfPieces={300} recycle={recycle} />
-            <h1 className="text-4xl font-bold">Discount Unlocked</h1>
-            <h2 className="text-8xl font-bold my-4 mt-8">🥳</h2>
-            <p className="text-2xl pt-16 py-2 ">
-              Congrats! You have unlocked a discount!
-            </p>
-            <p className="text-2xl"> Offer Valid Till: 10th July 2024.</p>
-            <p className="text-2xl">
-              {' '}
-              <s>₹800</s> ₹499{' '}
-            </p>
-            <a href="https://web3ssh.dev/register">
-              <button className="p-[3px] relative my-4 mt-8">
-                <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
-                <div className="px-8 py-2  bg-black bg-opacity-70 rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent sm:text-3xl hover:font-bold text-xl">
-                  Join at ₹499
-                </div>
-              </button>
-            </a>
-          </div>
-          <div className="p-6  mx-8 rounded-lg shadow-md bg-white bg-opacity-0 my-4">
-            <HeroHighlight>
-              <motion.h1
-                initial={{
-                  opacity: 0,
-                  y: 20,
-                }}
-                animate={{
-                  opacity: 1,
-                  y: [20, -5, 0],
-                }}
-                transition={{
-                  duration: 0.5,
-                  ease: [0.4, 0.0, 0.2, 1],
-                }}
-                className="text-xl px-4  py-4 lg:text-xl font-bold text-white  leading-relaxed lg:leading-snug text-center mx-auto max-w-2xl "
-              >
-                Dont forget to use the referral code <br />
-                <Highlight className="text-white">{code}</Highlight>
-                <br /> while filling out the Google Form.
-              </motion.h1>
-            </HeroHighlight>
+          <div className="mb-24">
+            <div className="p-6 border mx-8 rounded-lg shadow-md bg-white bg-opacity-10 mt-32">
+              <Confetti numberOfPieces={300} recycle={recycle} />
+              <h1 className="text-4xl font-bold">Discount Unlocked</h1>
+              <h2 className="text-8xl font-bold my-4 mt-8">🥳</h2>
+              <p className="text-2xl pt-16 py-2 ">
+                Congrats! You have unlocked a discount!
+              </p>
+              <p className="text-2xl"> Offer Valid Till: 10th July 2024.</p>
+              <p className="text-2xl">
+                {' '}
+                <s>₹800</s> ₹499{' '}
+              </p>
+              <a href="https://web3ssh.dev/register">
+                <button className="p-[3px] relative my-4 mt-8">
+                  <div className="absolute inset-0 bg-gradient-to-r from-indigo-500 to-purple-500 rounded-lg" />
+                  <div className="px-8 py-2  bg-black bg-opacity-70 rounded-[6px]  relative group transition duration-200 text-white hover:bg-transparent sm:text-3xl hover:font-bold text-xl">
+                    Join at ₹499
+                  </div>
+                </button>
+              </a>
+            </div>
+            <div className="p-6  mx-8 rounded-lg shadow-md bg-white bg-opacity-0 my-4">
+              <HeroHighlight>
+                <motion.h1
+                  initial={{
+                    opacity: 0,
+                    y: 20,
+                  }}
+                  animate={{
+                    opacity: 1,
+                    y: [20, -5, 0],
+                  }}
+                  transition={{
+                    duration: 0.5,
+                    ease: [0.4, 0.0, 0.2, 1],
+                  }}
+                  className=" px-4  py-8 lg:text-xl font-bold text-white  leading-relaxed lg:leading-snug text-center mx-auto max-w-2xl"
+                >
+                  Dont forget to use the referral code <br />
+                  <Highlight className="text-white">{code}</Highlight>
+                  <br /> while filling out the Google Form.
+                </motion.h1>
+              </HeroHighlight>
+            </div>
           </div>
         </>
       ) : (
