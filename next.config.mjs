e75482fdb00afd216ee8b71b635690e0da6a7839
aahnik <daw@aahnik.dev>
2024-07-22 19:47:22 +0530

    Update brochure redirect links

--- next.config.mjs
+++ next.config.mjs
@@ -10,25 +10,25 @@ const nextConfig = {
       {
         source: '/pitch',
         destination:
-          'https://drive.google.com/file/d/1lpezW5KzsP82Ku5ECwhUA2tfBuGp1U5K/view?usp=drive_link',
+          'https://drive.google.com/file/d/1BFzPUQeP3Bg4bWRCpgaFdn0aDFhkdDEC/view?usp=drive_link',
         permanent: false,
       },
       {
         source: '/cfs',
         destination:
-          'https://drive.google.com/file/d/1qBIZWryww6nAM47n4d31E-kMNwu6Yevp/view?usp=drive_link',
+          'https://drive.google.com/file/d/1soWn87dbySO8v0jkYYRvnd-r4p8TLb41/view?usp=drive_link',
         permanent: false,
       },
       {
         source: '/promo',
         destination:
-          'https://drive.google.com/file/d/1kx2dvwie4yqe2rFAnEnteCadNF5g7Of_/view?usp=drive_link',
+          'https://drive.google.com/file/d/1UFZy_bTjjfhSDbQqvmb320T1tQrQJrbS/view?usp=sharing',
         permanent: false,
       },
       {
         source: '/brochure',
         destination:
-          'https://drive.google.com/file/d/19VHPzwg-9_S-TlF4yX5konH3Z9WZlRRV/view?usp=drive_link',
+          'https://drive.google.com/file/d/1fa5Zmy4cOSogwEWpy-PuJLb28t_OUOpa/view?usp=sharing',
         permanent: false,
       },
       {
