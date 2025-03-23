e9ec07b8bd8be772b70d10fe93cadbcea1303631
rejzzzz <156344567+rejzzzz@users.noreply.github.com>
2025-03-23 17:18:28 +0530

    fall back to next 14.2

--- src/app/archive2024/layout.tsx
+++ src/app/archive2024/layout.tsx
@@ -1,5 +1,5 @@
 import GradientBackground from '@components/GradientBackground';
-import { GeistSans } from 'geist/font/sans';
+// import { GeistSans } from 'geist/font/sans';
 import Navbar2024 from '@components/components2024/Navbar2024';
 import Footer2024 from '@components/components2024/Footer2024';
 
@@ -9,25 +9,23 @@ export default function ArchiveLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body>
-        {/* Gradient Background */}
-        <div
-          style={{
-            width: '100vw',
-            height: '100vh',
-            position: 'fixed',
-            zIndex: '-10',
-          }}
-        >
-          <GradientBackground />
-        </div>
+    <>
+      {/* Gradient Background */}
+      <div
+        style={{
+          width: '100vw',
+          height: '100vh',
+          position: 'fixed',
+          zIndex: '-10',
+        }}
+      >
+        <GradientBackground />
+      </div>
 
-        {/* Content */}
-        <Navbar2024 />
-            <div id="root">{children}</div>
-        <Footer2024 />
-      </body>
-    </html>
+      {/* Content */}
+      <Navbar2024 />
+      {children}
+      <Footer2024 />
+    </>
   );
 }
